7eb0f4189b4eba0c09f1d7225ecc569cfa0c9e61
Rustam Sharipov <rustamshar@inbox.ru>
2022-10-30 14:13:18 +0500

    Устранил предупреждения

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String inputExpression = sc.nextLine();
 
         System.out.println(calc(inputExpression));
     }
-    public static String calc(String input) {
+    public static String calc(String input) throws Exception {
         String result;
         int operand1, operand2;
         int value;
@@ -15,7 +15,7 @@ public class Main {
         Converter oper;
         String[] elementsOfExpression = input.split(" ");
 
-        if (elementsOfExpression.length != 3) return "Входные значения не удовлетворяют условию";
+        if (elementsOfExpression.length != 3) throw new Exception();
 
         CheckForRomanDigit check1 = new CheckForRomanDigit(elementsOfExpression[0]);
         CheckForRomanDigit check2 = new CheckForRomanDigit(elementsOfExpression[2]);
@@ -26,14 +26,14 @@ public class Main {
             oper = new Converter(elementsOfExpression[2]);
             operand2 = oper.getArabianDigit();
             if (operand1>0 && operand2>0 && operand1<11 && operand2<11) isRoman = true;
-            else return "Входные значения не удовлетворяют условию";
+            else throw new Exception();
         }
-        else if (elementsOfExpression[0].matches("[1-9]||10") &&
-                elementsOfExpression[2].matches("[1-9]||10")) {
+        else if (elementsOfExpression[0].matches("[1-9]|10") &&
+                elementsOfExpression[2].matches("[1-9]|10")) {
             operand1 = Integer.parseInt(elementsOfExpression[0]);
             operand2 = Integer.parseInt(elementsOfExpression[2]);
         }
-        else return "Входные значения не удовлетворяют условию";
+        else throw new Exception();
 
         value = calculate(operand1, operand2, elementsOfExpression[1]);
 
@@ -42,7 +42,7 @@ public class Main {
                 oper = new Converter(String.valueOf(value));
                 result = oper.getRomanDigit();
             }
-            else return "Входные значения не удовлетворяют условию";
+            else throw new Exception();
         }
         else result = String.valueOf(value);
 
@@ -84,7 +84,7 @@ public class Main {
         }
         int getArabianDigit() {
             int index = 0;
-            for (int i=0; i<=romanDigits.length; i++) {
+            for (int i = 0; i <= romanDigits.length; i++) {
                 if (romanDigit.equals(romanDigits[i])) {
                     index = i;
                     break;
@@ -94,7 +94,7 @@ public class Main {
         }
         String getRomanDigit() {
             String totalValue = null;
-            for (int i=0; i<=romanDigits.length; i++) {
+            for (int i = 0; i <= romanDigits.length; i++) {
                 if (Integer.parseInt(romanDigit) == i) {
                     totalValue = romanDigits[i];
                     break;
@@ -105,13 +105,12 @@ public class Main {
     }
 
     static int calculate(int operand1, int operand2, String operator) {
-        int value = switch (operator) {
+        return switch (operator) {
             case ("+") -> operand1 + operand2;
             case ("-") -> operand1 - operand2;
             case ("*") -> operand1 * operand2;
             case ("/") -> operand1 / operand2;
             default -> 0;
         };
-        return value;
     }
 }
